efc338a8125f3d9e486e29ec5789e849be7f6226
ingriid-ayaala <ingriidayaala@gmail.com>
2022-10-04 10:02:27 +0200

    Persona matriz [][] = {{pers1,pers2},{pers3,pers4}};

--- principal/src/principal/Principal.java
+++ principal/src/principal/Principal.java
@@ -17,6 +17,7 @@ static Persona arrayPersonas[]=new Persona[10];
         Persona pers1 = new Persona("Ingrid","Ruiz");
         Persona pers2 = new Persona("Emily","Ruiz");
         Persona pers3 = new Persona("Ana","Ruiz");
+        Persona pers4 = new Persona("Ingrid","Gomez");
         
         Persona personas[]= new Persona [3];
         personas[0]= pers1;
@@ -27,6 +28,22 @@ static Persona arrayPersonas[]=new Persona[10];
         for (int i = 0; i < personas.length; i++) {
             System.out.println("*" + personas[i]);
         }
+        
+            //Persona matriz[][]= new Persona[2][2];
+            Persona matriz [][] = {{pers1,pers2},{pers3,pers4}};
+//            matriz[0][0]= pers1;
+//            matriz[0][1]= pers2;
+//            matriz[1][0]= pers3;
+//            matriz[1][1]= pers4;
+            
+            
+            for (int i = 0; i < matriz.length; i++){//visualizar
+              
+                for (int j = 0; j < matriz[0].length; j++){   
+                     System.out.print("\t"+matriz[i][j]);
+                }
+                System.out.println("");
+            }
         
         
     }
